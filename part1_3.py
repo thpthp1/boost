8f00b0b7baa4f9016d2688b025d8c02eb6c618d5
Phan Trinh <phan181299@gmail.com>
2021-04-12 13:44:01 -0400

    Fix svm option

--- part1_3.py
+++ part1_3.py
@@ -13,12 +13,13 @@ if __name__ == '__main__':
     # Training
     W = W = [1] * len(y_train)
     prob  = svm_problem(W, y_train, x_train)
-    param = svm_parameter('-t 0')
+    param = svm_parameter('-t 0 -v 10')
     m_lin = svm_train(prob, param)
+    # p_label, p_acc, p_val = svm_predict(y_test, x_test, m_lin)
 
     prob  = svm_problem(W, y_train, x_train)
-    param = svm_parameter('-t 1 -d 5')
+    param = svm_parameter('-t 1 -d 5 -v 10')
     m_poly = svm_train(prob, param)
 
-    p_label, p_acc, p_val = svm_predict(y_test, x_test, m_poly)
-    print(p_label)
+    # p_label, p_acc, p_val = svm_predict(y_test, x_test, m_poly)
+    #print(p_label)
